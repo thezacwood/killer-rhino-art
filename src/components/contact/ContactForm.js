499c828b39103c2d7aac70ce6e15bed733effa6d
thezacwood <102834360+thezacwood@users.noreply.github.com>
2022-04-07 16:03:24 -0700

    Update ContactForm.js

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ContactForm = () =>
   <section>
-    <form name="contact" method="POST" data-netlify="true">
+    <form name="killer-rhino-contact" method="POST" data-netlify="true">
       <div className="fields">
         <div className="field half">
           <label htmlFor="name">Name</label>
